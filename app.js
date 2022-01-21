b02fd6ba54aa589203ee8cfb2ee4d54d53b5c9fe
Liis Normann <liis.normann@gmail.com>
2022-01-21 08:55:46 +0000

    Added UI controller via standard module pattern

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-// module pattern basic structure
-(function() {
+// standard module pattern
+//it will be UI controller
+const UIController = (function() {
     //declare private variables and functions
+    let text = 'Hello World';
 
+    const changeText = function() {
+        const element = document.querySelector('h1')
+        element.textContent = text
+    }
     return {
         //declare public variables and functions
+        callChangeText: function() {
+            changeText();
+            console.log(text);
+        }
     }
 })();
+
+UIController.callChangeText(); 
